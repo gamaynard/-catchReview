dbe8dc66e854d07e0bb6b7a4c6ed1adf763e27ae
gamaynard <galphonsemaynard@gmail.com>
2020-11-03 15:09:53 -0500

    Reformats .json list of lists into flat file (R data frame)

--- FY2019_DataCompiler.R
+++ FY2019_DataCompiler.R
@@ -29,6 +29,7 @@ options(scipen = 6, digits = 4) # eliminate scientific notation
 
 ## load up the packages we will need:  (uncomment as required)
 library(RJSONIO)
+library(XLConnect)
 ## ---------------------------
 
 ## load up our functions into memory
@@ -80,5 +81,79 @@ for(i in 1:length(fileList)){
   }
   Dealer=rbind(Dealer,partial)
 }
-
-
+## The next step is to turn the .json EM file into a dataframe to enable merging
+## it with other records
+## Rename the EM .json to EM_JSON and create a new empty EM data frame
+EM_JSON=EM
+EM=data.frame(
+  VTR=as.numeric(),
+  VESSEL=as.character(),
+  HAUL_NO=as.numeric(),
+  startTime=as.numeric(),
+  endTime=as.numeric(),
+  startLat=as.numeric(),
+  startLon=as.numeric(),
+  species=as.character(),
+  count=as.numeric(),
+  weight=as.numeric()
+)
+## Each trip is an item in a list
+## For each trip
+for(i in 1:length(EM_JSON)){
+  ## Open the list item
+  trip=EM_JSON[[i]]
+  ## Each trip is a list of variables and dataframes
+  ## Extract the VTR number
+  vtr=trip$trip_id
+  ## Extract the vessel name
+  vessel=trip$vessel_name
+  ## Extract the number of hauls
+  hauls=trip$total_hauls
+  ## Report discards haul by haul
+  for(h in 1:hauls){
+    haul=trip$hauls[[h]]
+    startTime=haul[2]
+    endTime=haul[3]
+    startLat=haul[4]
+    startLon=haul[5]
+    ## Discards are listed by species
+    discards=haul$discards
+    for(d in 1:length(discards)){
+      species=discards[[d]]$species
+      count=discards[[d]]$count_discarded
+      weight=discards[[d]]$pounds_discarded
+      newline=data.frame(
+        VTR=as.numeric(),
+        VESSEL=as.character(),
+        HAUL_NO=as.numeric(),
+        startTime=as.numeric(),
+        endTime=as.numeric(),
+        startLat=as.numeric(),
+        startLon=as.numeric(),
+        species=as.character(),
+        count=as.numeric(),
+        weight=as.numeric()
+      )
+      newline[1,]=NA
+      newline$VESSEL=as.character(newline$VESSEL)
+      newline$species=as.character(newline$species)
+      newline$VTR=vtr
+      newline$VESSEL=toupper(
+        as.character(vessel)
+      )
+      newline$HAUL_NO=h
+      newline$startTime=startTime
+      newline$endTime=endTime
+      newline$startLat=startLat
+      newline$startLon=startLon
+      newline$species=toupper(
+        as.character(species)
+      )
+      newline$count=count
+      newline$weight=weight
+      EM=rbind(EM,newline)
+      rm(newline)
+    }
+  }
+}
+## 
